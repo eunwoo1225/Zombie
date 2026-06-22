061a0431e07f61247089e55bd61d1b06c3e39fed
eunwoo1225 <eunwoo122518@gmail.com>
2026-06-22 12:40:26 +0900

    20260622
    - PlayerShooter 스크립트 작성

--- Assets/Scripts/GunData.cs
+++ Assets/Scripts/GunData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class GunData : ScriptableObject
 {
     public AudioClip shotClip; // 발사 소리
-    public AudioClip AudioClip; // 재장전 소리
+    public AudioClip reloadClip; // 재장전 소리
 
     public float damage = 25; // 공격력
 
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public void UpdateAmmoText(int a, int b)
+    {
+    }
+}
