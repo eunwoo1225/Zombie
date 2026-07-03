b8345f66724edc9371ad9e3824cf840a2f737ae3
eunwoo1225 <eunwoo122518@gmail.com>
2026-07-03 12:38:44 +0900

    20260703
    - Zombie 오브젝트 컴포넌드 설정
    - UI 캔버스 및 스크립트 추가
    - GameManager 스크립트 작성

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,10 +1,57 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // 씬 관리자 관련 코드
+using UnityEngine.UI; // UI 관련 코드
 
+// 필요한 UI에 즉시 접근하고 변경할 수 있도록 허용하는 UI 매니저
 public class UIManager : MonoBehaviour
 {
-    public static UIManager instance;
+    // 싱글턴 접근용 프로퍼티
+    public static UIManager instance
+    {         get
+        {
+            if (m_instance == null)
+            {
+                m_instance = FindFirstObjectByType<UIManager>();
+            }
+            return m_instance;
+        }
+    }
 
-    public void UpdateAmmoText(int a, int b)
+    private static UIManager m_instance; // 싱글턴이 할당될 변수
+
+    public Text ammoText; // 탄알 표시용 텍스트
+    public Text scoreText; // 점수 표시용 텍스트
+    public Text waveText; // 적 웨이브 표시용 텍스트
+    public GameObject gameoverUI; // 게임오버 시 활성화할 UI
+
+    // 탄알 텍스트 갱신
+    public void UpdateAmmoText(int magAmo, int remainAmmo)
     {
+        ammoText.text =magAmo + "/" + remainAmmo;
+    }
+
+    // 점수 텍스트 갱신
+    public void UpdateScoreText(int newScore)
+    {
+        scoreText.text = "Score : " + newScore;
+    }
+
+    // 적 웨이브 텍스트 갱신
+    public void UpdateWaveText(int waves, int count)
+    {
+        waveText.text = "Wave : " + waves + "\nEnemy Left : " + count;
+    }
+
+    // 게임오버 UI 활성화
+    public void SetActiveGameoverUI(bool active)
+    {
+        gameoverUI.SetActive(active);
+    }
+
+    // 게임 재시작
+    public void GameRestart()
+    {
+        // 현재 활성화된 씬을 다시 로드
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
